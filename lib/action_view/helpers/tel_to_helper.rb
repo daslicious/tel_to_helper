a5cc959409d43729935ed27902c31db399d0b227
Damien <damiennews@gmail.com>
2016-01-08 11:56:36 -0800

    add area_code to default options

--- lib/action_view/helpers/tel_to_helper.rb
+++ lib/action_view/helpers/tel_to_helper.rb
@@ -7,7 +7,7 @@ module ActionView
       # Creates a telephone link tag of the given +number+ and +name+. If +nil+ 
       # is passed as the +name+ the value of the link itself will become the 
       # +number+. +options+ and +html_options+ will be same as +link_to.
-      def tel_to(number, name = nil, link_to_options = nil)
+      def tel_to(number, name = nil, link_to_options = {area_code: true})
         link_to(h(name || number), "tel:#{h(number)}", link_to_options) if number.present?
       end
     end
